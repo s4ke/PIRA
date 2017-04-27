c557dfdf47e1442f8043f6bf3b219a40244b9b80
s4ke <martinbraun123@aol.com>
2017-04-27 13:53:01 +0200

    threaded program

--- code/src/uebung1/threaded.cpp
+++ code/src/uebung1/threaded.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <thread>
+#include <list>
+#include <vector>
 
 const long N = 999999999;
+const int defaultThreads = 2;
 
 long sumUp(long N) {
     long sum = 0;
-    for(long i = 0; i < N; ++i) {
+    for (long i = 0; i < N; ++i) {
         sum += i;
     }
     return sum;
@@ -16,21 +19,36 @@ void run(long N) {
     std::cout << "sum from 1 to " << N << " is " << sumUp(N) << std::endl;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     long actualN = 0;
-    std::cout << "running sumUp twice on two threads" << std::endl;
-    if(argc <= 1) {
+    long coreCount = 0;
+    std::cout << "running sumUp twice on multiple threads" << std::endl;
+    if (argc <= 1) {
         std::cout << "using default value for N: " << N << std::endl << std::flush;
         actualN = N;
     } else {
-        std::cout << "using user supplied value of " << argv[1] << std::endl;
+        std::cout << "using user supplied value of N: " << argv[1] << std::endl;
         actualN = atol(argv[1]);
     }
 
-    std::thread t1(run, N);
-    std::thread t2(run, N);
+    if (argc <= 2) {
+        std::cout << "using default value for #threads: " << defaultThreads << std::endl << std::flush;
+        coreCount = defaultThreads;
+    } else {
+        std::cout << "using user supplied number of cores" << argv[2] << std::endl;
+        coreCount = atoi(argv[2]);
+    }
 
-    t1.join();
-    t2.join();
+    std::vector<std::thread> threads;
+    threads.reserve(coreCount);
+
+    for (int i = 0; i < coreCount; ++i) {
+        threads.emplace_back( run, actualN ) ;
+    }
+
+    for (auto &t : threads) {
+        t.join();
+    }
+
     return 0;
 }
