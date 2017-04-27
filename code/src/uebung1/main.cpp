5e8f1d40a32d412f8d511a6ef1f63b21d560d7d8
s4ke <martinbraun123@aol.com>
2017-04-27 13:07:02 +0200

    minor

--- code/src/uebung1/main.cpp
+++ code/src/uebung1/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cstdlib>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const long N = 4999999999;
+
+long sumUp(long N) {
+    long sum = 0;
+    for(long i = 0; i < N; ++i) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main(int argc, char** argv) {
+    if(argc <= 1) {
+        std::cout << "using default value for N: " << N << std::endl << std::flush;
+        std::cout << "sum from 1 to " << N << " is " << sumUp(N) << std::endl;
+    } else {
+        std::cout << "sum from 1 to " << argv[1] << " is " << sumUp(atol(argv[1])) << std::endl;
+    }
     return 0;
 }
